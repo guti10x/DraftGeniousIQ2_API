9c06fe6e617eaf55db6a308417d2c464bba74d6d
Daniel Gutiérrez Torres <82153822+guti10x@users.noreply.github.com>
2024-03-30 12:47:37 +0100

    Add métodos POST Y DELETE de estadísticas de equipos (añadir estadística asociada a un equipo por su id + eliminar TODAS las estadísticas asociadas a un equipo)

--- app/Http/Controllers/EstadisticasEquiposController.php
+++ app/Http/Controllers/EstadisticasEquiposController.php
@@ -58,4 +58,61 @@ class EstadisticasEquiposController extends Controller
             return response()->json(['mensaje' => 'No se encontraron estadísticas para el equipo con el ID proporcionado'], 404);
         }
     }
+
+
+    /**
+     * Crea nuevas estadísticas asociadas a un equipo utilizando los datos recibidos en la petición
+     * /api/estadisticas-equipos
+     * {
+     *   "id_equipo": id_del_equipo_al_que_le_pertenecen_las_estadisticas_a_continuación,
+     *   "puntos": puntos_totales_obtenidos_del_equipo,
+     *   "media_puntos_jornada": media_puntos_obtenidos_por_jornada,
+     *   "valor": valor_del_equipo,
+     *   "num_jugadores": número_jugaodres_que_tiene_el_equipo
+     *  }
+     */
+
+     public function store(Request $request)
+     {
+         $request->validate([
+             'id_equipo' => 'required',
+             'puntos' => 'required',
+             'media_puntos_jornada' => 'required',
+             'valor' => 'required',
+             'num_jugadores' => 'required',
+         ]);
+     
+         $requestData = $request->all();
+         $requestData['created_at'] = now(); // Establecer el valor de created_at como la fecha y hora actual
+     
+         $estadisticasEquipo = new EstadisticasEquipos();
+         $estadisticasEquipo->fill($requestData)->save();
+     
+         return response()->json($estadisticasEquipo, 201);
+     }
+
+
+     /**
+     * Eliminar TODAS las estadisticas asociadas a un equipo por su ID.
+     * api/estadisticas-equipos/{id}
+     */
+    public function destroyAllStatsByTeamId($id_equipo)
+{
+    // Buscar todas las estadísticas del equipo por su ID
+    $estadisticasEquipos = EstadisticasEquipos::where('id_equipo', $id_equipo)->get();
+
+    // Verificar si se encontraron estadísticas para ese equipo
+    if ($estadisticasEquipos->isEmpty()) {
+        // Devolver un mensaje de error si no se encontraron estadísticas para ese equipo
+        return response()->json(['mensaje' => 'No se encontraron estadísticas para el equipo con el ID proporcionado'], 404);
+    }
+
+    // Eliminar todas las estadísticas encontradas
+    foreach ($estadisticasEquipos as $estadisticas) {
+        $estadisticas->delete();
+    }
+
+    // Devolver un mensaje de éxito
+    return response()->json(['mensaje' => 'Todas las estadísticas del equipo han sido eliminadas correctamente'], 200);
+}
 }
--- routes/api.php
+++ routes/api.php
@@ -14,17 +14,16 @@ Route::get('/user', function (Request $request) {
 Route::get('/equipos', [EquiposController::class, 'index']); // Obtener todos los equipos
 Route::get('/equipos/{equipo}', [EquiposController::class, 'getNameById']); // Obtener NAME de un equipo por su ID
 Route::get('/equipos/id/{nombre}', [EquiposController::class, 'getIdByName']);// Obtener ID de un equipo por su NAME
-Route::post('/equipos', [EquiposController::class, 'store']); // Crear un nuevo equipo pasandole su nombre
-Route::delete('/equipos/{id}', [EquiposController::class, 'destroy']);
-Route::put('/equipos/{equipos}', [EquiposController::class, 'update']);
+Route::post('/equipos', [EquiposController::class, 'store']); // Crear un nuevo equipo pasándole su nombre
+Route::delete('/equipos/{id}', [EquiposController::class, 'destroy']); //Eliminar un equipo
+Route::put('/equipos/{equipos}', [EquiposController::class, 'update']); //Actualizar nombre d eun equipo
 
-# ESTADíSTICAS_EQUIPOS:
-//Obtener TODAS las estadísticas de todos los equipos
-Route::get('/estadisticas-equipos', [EstadisticasEquiposController::class, 'index']);
-//Obtener TODAS las estadísticas asociadas a un ID de un equipo
-Route::get('/estadisticas-equipos/{id}/stats', [EstadisticasEquiposController::class, 'getStatsById']);
-//Obtener ÚLTIMA estadística asociada a un ID de un equipo
-Route::get('/estadisticas-equipos/{id}/last-stats', [EstadisticasEquiposController::class, 'getLastStatsById']);
+# ESTADÍSTICAS_EQUIPOS:
+Route::get('/estadisticas-equipos', [EstadisticasEquiposController::class, 'index']); //Obtener TODAS las estadísticas de todos los equipos
+Route::get('/estadisticas-equipos/{id}/stats', [EstadisticasEquiposController::class, 'getStatsById']); //Obtener TODAS las estadísticas asociadas a un ID de un equipo
+Route::get('/estadisticas-equipos/{id}/last-stats', [EstadisticasEquiposController::class, 'getLastStatsById']); //Obtener ÚLTIMA estadística asociada a un ID de un equipo
+Route::post('/estadisticas-equipos', [EstadisticasEquiposController::class, 'store']); // Crear una nueva estadítica asociada un equipo
+Route::delete('/estadisticas-equipos/{id_equipo}/all', [EstadisticasEquiposController::class, 'destroyAllStatsByTeamId']); // Ruta para eliminar todas las estadísticas asociadas a un equipo por su ID
 
 #EQUIPOS - ESTADíSTICAS_EQUIPOS:
-Route::get('/equipos/{equipo}/estadisticas', [EstadisticasEquiposController::class, 'index']);
+Route::get('/equipos/{equipo}/estadisticas', [EstadisticasEquiposController::class, 'index']); //Obtener TODAS las estadísticas asociadas a un equipo por su id
